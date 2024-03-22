caab8bebb0d6094f10f0695b873443ad21dcb92d
cesar <admin@angularfrontenders.com>
2024-03-22 17:49:35 +0100

    feat: empty buttons library

---
+++ libs/buttons/src/lib/buttons/buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'af-buttons',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './buttons.component.html',
+  styleUrl: './buttons.component.scss',
+})
+export class ButtonsComponent {}
